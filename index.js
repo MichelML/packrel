8f32c63388f35f01ee8537221f83344f9687f087
MichelML <michmoreau.l@gmail.com>
2018-02-15 10:47:35 -0500

    replace bumType

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const execSync = require('child_process').execSync;
 const execSyncOptions = {encoding: 'utf-8'};
 const releaseType = process.argv[process.argv.length - 1].toLowerCase();
 
-if (['patch', 'minor', 'major'].indexOf(bumpType) < 0) {
+if (['patch', 'minor', 'major'].indexOf(releaseType) < 0) {
   throw('Wrong type of release provided. You must specify a patch, minor, or major release.');
   process.exit(1);
 }
